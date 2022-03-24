dd16e3126026580488d480d54ed2ba803ee3c16e
lbrad24 <97545962+lbrad24@users.noreply.github.com>
2022-03-24 11:12:29 -0600

    fixed loadloganurl.test.js file

--- utils/loadloganurl.mjs
+++ utils/loadloganurl.mjs
@@ -1,16 +1,17 @@
 import fetch from 'node-fetch';
 
-const loodloganurl = async () => {
+const loadloganurl = async () =>{
 
-    const option = {
+    const options = {
         method: 'GET',
         headers: {'Content-Type':'application/json'}
-    },
+    };
 
-    const apiResponse = await fetch('http://localhost:3000/logan', option);
-    const apiResponse = await apiResponse.text();
+    const apiResponse = await fetch('http://localhost:3000/logan', options);
+    const testResponse = await apiResponse.text();
+    //just a comment
+    return testResponse;
 
-    return loginResponse;
-}
+};
 
-export loodloganurl;
+export default loadloganurl;
