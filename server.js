eb633f918200438b5e963e80f8895fdce6e02d45
Candyraider7th <pal19028@byui.edu>
2022-03-24 11:01:30 -0600

    server changes for alex

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const express = require('express');
+import express from 'express';
 
 const port = 3000;
 
 const app = express();
 
 app.get('/', (req,res) => {res.send("Hello Browser")});
+
+app.get('/alex', (req,res) => {res.send("Hello Alex")});
 
 app.listen(port, ()=>console.log("Listening"));
