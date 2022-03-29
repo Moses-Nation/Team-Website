0c1dae430683c2dd5b7546b9958af19b498b536e
unknown <devadanamkaratapu@gmail.com>
2022-03-29 11:34:40 -0600

    changed my port number in my pod.yaml

--- utils/loaddaveurl.mjs
+++ utils/loaddaveurl.mjs
@@ -7,11 +7,11 @@ const daveurl = async ()=>{
         headers: {'Content-Type':'aplication/json'}
     };
 
-    const apiResponse = await fetch ('http://localhost:3000/dave', options)
+    const apiResponse = await fetch ('http:///dave', options)
     const testResponse = await apiResponse.text();
 
     return testResponse;
 
 };
 
-export default loaddaveurl;
+export default daveurl;
