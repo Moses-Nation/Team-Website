4dd8960e6aabef84f799f35c67b81a1217249067
unknown <devadanamkaratapu@gmail.com>
2022-03-29 17:07:03 -0600

    made a change to test my pod.yaml

---
+++ test/loaddave.test.js
@@ -0,0 +1,9 @@
+import loaddaveurl from '../utils/loaddaveurl.mjs';
+import assert from 'assert';
+
+it("Tests Dave URL Endpoint", async ()=>{
+
+    const urlResponse = await loaddaveurl();
+    console.log("URL Response:")+urlResponse;
+    assert.equal(urlResponse, "Hello Dave");
+});
